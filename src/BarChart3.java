5d17f35d6ec2c07b77c9a9603051242d5397d14a
marc6250 <marc6250@ISS2V0SKH2.scdsb.pri>
2019-09-12 11:35:50 -0400

    More changes

--- src/BarChart3.java
+++ src/BarChart3.java
@@ -1,5 +1,7 @@
 
 import TurtleGraphics.Pen;
+import TurtleGraphics.SketchPadWindow;
+import TurtleGraphics.StandardPen;
 import java.awt.Color;
 import java.util.Scanner;
 
@@ -7,30 +9,33 @@ public class BarChart3 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int length;
-        for (int i = 0; i < 4; i++) {
-            while (true) {
-                System.out.print("Enter length for bar " + (i + 1) + " (max 600) > ");
-                length = s.nextInt();
-                if (length <= 600 && length >= 10) {
-                    break;
-                } else {
-                    System.out.println("Error, number must be lower than 600\n");
-                }
-            }
+        PenBar pb1 = new PenBar();
+        PenBar pb2 = new PenBar();
+        PenBar pb3 = new PenBar();
+        PenBar pb4 = new PenBar();
 
-            makeBar(p, pb1, -350, 200, pb1.width, Color.blue);
-            makeBar(p, pb1, -350, 100, pb2.width, Color.red);
-            makeBar(p, pb1, -350, 0, pb3.width, Color.green);
-            makeBar(p, pb1, -350, -100, pb4.width, Color.orange);
-        }
+        System.out.print("Enter length for bar 1 (max 600) > ");
+        pb1.width = s.nextInt();
+        System.out.print("Enter length for bar 2 (max 600) > ");
+        pb2.width = s.nextInt();
+        System.out.print("Enter length for bar 3 (max 600) > ");
+        pb3.width = s.nextInt();
+        System.out.print("Enter length for bar 4 (max 600) > ");
+        pb4.width = s.nextInt();
+
+        Pen p = new StandardPen(new SketchPadWindow(800, 600));
+
+        makeBar(p, pb1, -350, 200, pb1.width, Color.blue);
+        makeBar(p, pb2, -350, 100, pb2.width, Color.red);
+        makeBar(p, pb3, -350, 0, pb3.width, Color.green);
+        makeBar(p, pb4, -350, -100, pb4.width, Color.orange);
     }
 
-    public void makeBar(Pen p, PenBar pb, int x, int y, int w, Color c) {
+    public static void makeBar(Pen p, PenBar pb, int x, int y, int w, Color c) {
         pb.x = x;
         pb.y = y;
         pb.width = w;
-        pb.barColor = c;
+        pb.barCol = c;
         pb.draw(p);
     }
 
--- src/PenBar.java
+++ src/PenBar.java
@@ -4,5 +4,17 @@ import TurtleGraphics.StandardPen;
 import java.awt.Color;
 
 public class PenBar {
-    Pen p = new StandardPen(new SketchPadWindow(800,600));
+    int x,y;
+    int width;
+    Color barCol;
+    public void draw(Pen p){
+        p.up();
+        p.move(x,y);
+        p.setDirection(0);
+        p.down();
+        p.setWidth(50);
+        p.move(width);
+        p.drawString("Value: " + width);
+    }
+    
 }
