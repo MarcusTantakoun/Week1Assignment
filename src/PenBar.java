6dc739d2e903c06bc405c934d1b26793906f27da
marc6250 <marc6250@ISS2V0SKH2.scdsb.pri>
2019-09-12 11:48:51 -0400

    More changes

--- src/PenBar.java
+++ src/PenBar.java
@@ -8,12 +8,16 @@ public class PenBar {
     int width;
     Color barCol;
     public void draw(Pen p){
+        p.setColor(barCol);
         p.up();
         p.move(x,y);
         p.setDirection(0);
         p.down();
         p.setWidth(50);
         p.move(width);
+        p.up();
+        p.move(50);
+        p.down();
         p.drawString("Value: " + width);
     }
     
